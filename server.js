e9c5495b3168abc28a399b7f23809cc25ee76dad
Omar Ilpa <omarilpa09@gmail.com>
2025-09-26 00:48:21 +0200

    adding the visitor email for viewing the AdminDash

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(
     origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning', 'Access-Control-Allow-Origin'],
   })
 );
 app.use("/api/auth", authRouter);
