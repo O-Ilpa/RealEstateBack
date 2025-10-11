e10acbce09edb52ee89829cf2bbb1319056f0771
Omar Ilpa <omarilpa09@gmail.com>
2025-10-11 14:05:33 +0200

    adding ping route to keep the server awake

--- server.js
+++ server.js
@@ -13,22 +13,30 @@ app.use(express.json());
 const allowedOrigins = [
   process.env.LOCAL_FRONT_ENDPOINT,
   process.env.FRONT_END_ENDPOINT,
+  "localhost:8000",
 ];
-
+const pinger = express.Router();
+pinger.get("/ping", (req, res) => {
+  return res.json({ success: true });
+});
 app.use(
   cors({
     origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning', 'Access-Control-Allow-Origin'],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "ngrok-skip-browser-warning",
+      "Access-Control-Allow-Origin",
+    ],
   })
 );
 app.use("/api/auth", authRouter);
 app.use("/api/properties", propertyRouter);
 
 mongoose.connect(process.env.DATABASE_URL);
-const db =  mongoose.connection;
-db.on("error", (error) => console.error(error.name)); 
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error.name));
 db.once("open", () => console.log("connected to mongoose"));
 app.listen(process.env.PORT || 5000);
- 
